508e88ff431791bff8945548549d4e5d3ae81c88
savas097 <https://savas097@git.tu-berlin.de>
2021-08-12 13:52:30 +0200

    first commit - reverse words with length >= 5

---
+++ word_spin.py
@@ -0,0 +1,17 @@
+
+# Write a function that takes in a string of one or more words
+# and returns the same string, but with all five or more letter words reversed
+
+
+def reverse(sentence):
+    reversed_sentence = ""
+    for word in sentence.split():
+        if len(word) >= 5:
+            reversed_sentence += word[::-1] + " "
+        else:
+            reversed_sentence += word + " "
+    print(reversed_sentence)
+
+
+if __name__ == '__main__':
+    reverse(input("Input your Sentence: "))
