894ef47b2b37885424aa97d6d9daf4294a64ec35
savas-grossmann <https://savas097@git.tu-berlin.de>
2021-08-20 21:42:02 +0200

    reverse String leetcode 344

---
+++ reverse_string.py
@@ -0,0 +1,10 @@
+def reverseString(s):
+    for i in range(int(len(s) / 2)):
+        s[i], s[len(s) - i - 1] = s[len(s) - i - 1], s[i]
+    return s
+
+
+if __name__ == '__main__':
+    word = ["h", "a", "l", "l", "o"]
+    print(word)
+    print(reverseString(word))
